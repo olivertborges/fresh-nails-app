ac9e2881ed17da4be60e657e0d0f37b9e5788e54
Olivert Borges <65991553+olivertborges@users.noreply.github.com>
2026-05-15 09:32:09 -0300

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Toaster } from 'react-hot-toast';
+import emailjs from '@emailjs/browser';
+import { EMAILJS_CONFIG } from './config/emailjs';
 import App from './App';
 import './index.css';
 
+// Inicializar EmailJS UNA SOLA VEZ al inicio de la app
+emailjs.init(EMAILJS_CONFIG.PUBLIC_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Toaster position="top-right" />
     <App />
   </React.StrictMode>
 );
